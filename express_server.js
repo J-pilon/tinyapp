3d8fa6429f98bcf5fbc42478a42176b86eccd15f
Josiah <josiah.pilon@yahoo.ca>
2021-03-15 21:35:13 +0000

    add handlers for /hello and /urls.json

--- express_server.js
+++ express_server.js
@@ -11,6 +11,14 @@ app.get('/', (request, response) => {
   response.send('Hello');
 });
 
+app.get('/urls.json', (request, response) => {
+  response.json(urlDatabase);
+});
+
+app.get('/hello', (request, response) => {
+  response.send('<html><body>Hello <b>World</b></body></html>\n')
+});
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`)
 });
